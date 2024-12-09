e07c8d8cd63a81329a37203e151e418543175938
RahidAmin <rahidamin19@gmail.com>
2024-12-09 22:56:04 +0600

    crud added

---
+++ ___steps__.js
@@ -0,0 +1,16 @@
+/***
+ * --------Mongodb connecton------
+ * 1.create an account.
+ * 2.write an user with password.
+ * 3.whitelist ip address.
+ * 4.database>connect>driver>node>show all code
+ * 5.Change the password in the uri.
+ * 
+ * ------------express---
+ * 1.create ---post
+ * 2.app.post()
+ * 3.make the function async to use await inside it;
+ * 4.access data from the body:const user=user.body;
+ *
+ * 
+ * **/
